6931924e79b45f8be4f231bbefbbc4cfd5c84e19
Pranay Bansal <pranay.gfg@gmail.com>
2025-10-26 17:38:49 +0530

    class-2

--- class-1/examples.cpp
+++ class-1/examples.cpp
@@ -1,3 +1,6 @@
+
+// TC: O(A + B)
+// AS: O(1)
 for (int i = 0; i < A.size(); i++) { 
     cout << A[i] << endl;
 }
@@ -7,6 +10,8 @@ for (int i = 0; i < B.size(); i++) {
 
 // ---------------------------------------------------
 
+// TC: O(A*B)
+// AS: O(1)
 for (int i = 0; i < A.size(); i++) {
     for (int j = 0; j < B.size(); j++) {
         cout << A[i] + B[j] << endl;
@@ -15,6 +20,8 @@ for (int i = 0; i < A.size(); i++) {
 
 // ---------------------------------------------------
 
+// TC: O(n^2)
+// AS: O(1)
 void func(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
@@ -25,7 +32,8 @@ void func(int n) {
 
 // ---------------------------------------------------
 
-// TC: O(n^2 * len(s))
+// TC: O(m * n^2) where m is the length of the string.
+// AS: O(1)
 void func(int n, string s) {
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
@@ -36,6 +44,9 @@ void func(int n, string s) {
 
 // ---------------------------------------------------
 
+// Input Space: O(m^2)
+// Output Space: O(k)
+// Aux Space: O(n)
 vector<int> func(vector<vector<int>> arr) { // arr: m^2, output: k
     // ....
     vector<int> temp(n);
@@ -48,6 +59,8 @@ vector<int> func(vector<vector<int>> arr) { // arr: m^2, output: k
 
 // ---------------------------------------------------
 
+// Input Space: O(m * max_length)
+// Aux Space: O(n * max_length)
 string func(vector<string> arr) { // arr: m
     // ...
     vector<string> temp(n);
